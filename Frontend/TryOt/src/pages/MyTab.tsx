8fd459515594a8c23a82ce1967858014f73fea1a
grace <gracekim027@gmail.com>
2023-11-02 11:45:14 +0900

    [feat] add mypage view

--- Frontend/TryOt/src/pages/MyTab.tsx
+++ Frontend/TryOt/src/pages/MyTab.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
+//해야하는 기능:
+//1. Change Password
+//2. Turn on/off gpt refinement
+//3. See Recent Items Viewed
 function MyTab() {
-  return (
-    <View>
-      <Text>MyTab</Text>
-    </View>
-  );
+    return (
+        <View>
+            <View>
+                <Text>Username</Text>
+                <Text>Email</Text>
+            </View>
+            <View
+                style={{
+                    height: 10, // Adjust the height as needed
+                    backgroundColor: 'gray',
+                }}
+            />
+            <View>
+                <Text>Turn on/off GPT refinement</Text>
+            </View>
+            <View>
+                <Text>Gender</Text>
+            </View>
+            <View>
+                <Text>Change Password</Text>
+            </View>
+            <View>
+                <Text>Logout</Text>
+            </View>
+            <View
+                style={{
+                    height: 10, // Adjust the height as needed
+                    backgroundColor: 'gray',
+                }}
+            />
+        </View>
+    );
 }
 
 export default MyTab;
