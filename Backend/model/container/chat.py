d1472e383e2bd8ad286d81d56cabd5c62a8d8708
ada-flo <kveldsstjerne@snu.ac.kr>
2023-11-04 19:18:48 +0900

    [feat] engineering prompt for gpt model used in chat

---
+++ Backend/model/container/chat.py
@@ -0,0 +1,69 @@
+import os
+import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain, LLMChain
+from langchain.prompts import (
+    ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, AIMessagePromptTemplate, MessagesPlaceholder
+    )
+from langchain.memory import ConversationBufferMemory
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import StructuredOutputParser
+
+# Authenticate with OpenAI API key
+openai.api_key = os.environ['OPENAI_API_KEY']
+llm = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo-0301")
+
+# Define schemas
+response_schemas = [
+    ResponseSchema(name="answer", description="Answer to the human's question. This is the only part that will be visible to the human."),
+    ResponseSchema(name="human_query", description="A short phrase that can encapsulate what the human wants"),
+    ResponseSchema(name="new_fashion_items", description="A 0 or 1 flag that ")
+    ResponseSchema(name="fashion_items", description="list of fashion items generated")
+]
+
+output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+format_instructions = output_parser.get_format_instructions()
+
+# Define templates
+system_template = """
+You are a helpful and friendly chatbot designed to help humans with shopping the fashion items they want.
+The human will talk to you to in the hope that you will suggest detailed and concrete fashion items that suits his/her needs.
+Your output should be of a json format. More infomation will follow later about how to format your output.
+
+First, you should interact with the human and answer his/her questions as this helpful chatbot assistant you are. 
+Your response for this first part should go into the "answer" part of the final json output format.
+Second, you need to summarize into a short phrase what the user wants.
+Your response for this second part should go into the "human_query" part of the final json output format.
+Third, once you have collected enough information about what the user needs, you should give a list of fashion items that suits the human's needs.
+Put this into the "fashion_items" part of the final json output format.
+Fourth,
+"""
+
+human_template = """
+{question}
+"""
+
+ai_template = """
+"""
+
+format_template = """{format_instructions}"""
+
+# Construct prompt from templates
+prompt = ChatPromptTemplate(
+    messages = [
+        SystemMessagePromptTemplate.from_template(system_template),
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template(human_template),
+        AIMessagePromptTemplate.from_template(ai_template),
+        SystemMessagePromptTemplate.from_template(format_template)
+    ],
+    input_variables={"question"},
+    partial_variables={"format_instructions": format_instructions}
+)
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+chain = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
+output_dict = output_parser.parse(chain.predict(question="what should i wear for high school reunion"))
+print(output_dict.get("answer"))
+print(output_dict.get("query"))
+print(output_dict.get("fashion_items"))
