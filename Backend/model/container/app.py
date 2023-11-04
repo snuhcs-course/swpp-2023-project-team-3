29510d43e0ce04dbb8bc5f902dae617f5f03f926
covv09on9 <kwj970712@snu.ac.kr>
2023-11-04 18:16:55 +0900

    [feat] FashionCLIP-GPT model adds a feature to store the search log to log DB.

--- Backend/model/container/app.py
+++ Backend/model/container/app.py
@@ -1,4 +1,4 @@
-from flask_restx import Api, Resource, reqparse
+# from flask_restx import Api, Resource, reqparse
 from typing import List
 import os
 import json
@@ -6,32 +6,24 @@ from flask import Flask, jsonify, request, Response
 from inference import ClipTextEmbedding
 from datetime import datetime
 from gpt import GPT
+import requests
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='API 문서', description='Swagger 문서', doc="/api-docs")
-test_api = api.namespace('test', description='조회 API')
+# api = Api(app, version='1.0', title='API 문서', description='Swagger 문서', doc="/api-docs")
+# test_api = api.namespace('test', description='조회 API')
 
 fclip = ClipTextEmbedding()
 gpt = GPT()
 
-def inputQuery(queryList:List[str])->List[int]:
-    itemdf = fclip.get_similarity(queryList)
-    print(itemdf.info())
-    cols = itemdf.columns.tolist()
-    itemdf['sum'] = itemdf[cols[1:]].sum(axis=1)
-    itemdf = itemdf.sort_values(by="sum", ascending=False)
-    itemIDList = itemdf['id'].tolist()
-    return itemIDList
-
-@test_api.route('/')
-class Test(Resource):
-    def get(self):
-        health = fclip._check_model()
-        status = 200 if health else 404
-        return Response(response="\n", status=status, mimetype="application/json")
+# @test_api.route('/')
+# class Test(Resource):
+#     def get(self):
+#         health = fclip._check_model()
+#         status = 200 if health else 404
+#         return Response(response="\n", status=status, mimetype="application/json")
     
-    def post(self):
-        return Response(response=output, status=status, mimetype="application/json")
+#     def post(self):
+#         return Response(response=output, status=status, mimetype="application/json")
 
 @app.route("/ping", methods=["GET"])
 def ping():
@@ -40,91 +32,58 @@ def ping():
     return Response(response="\n", status=status, mimetype="application/json")
 
 @app.route("/invocations", methods=["POST"])
-def predict():
+async def predict():
     data = None
     queryList = [] # query를 담을 list
-    responseQueryList = []
-    itemIDList = []
     if request.content_type == "application/json":
         """
         request body should be :
         {
             "user_id" : 1203,
-            "gpt_usable" : 0 | 1, Number
-            "text" : ["A-Line Dresses and Skirts"] // first index of list is user query for default
-            "target_index" : [1, 0|1, 0|1, 0|1]
+            "text" : "A-Line Dresses and Skirts", String
         }
         """
         data = request.data.decode("utf-8")
         inputs = json.loads(data)
-        queryList.append(inputs['text'][0])
-        responseQueryList.append(inputs['text'][0])
-        gptUsable = inputs['gpt_usable']
+        queryList.append(inputs['text'])
         user_id = inputs['user_id']
-        targetIndex = inputs['target_index']
         timestamp = int(datetime.now().timestamp())
     else :
         return Response(
             response="This predictor only supports for plain-text in English", status=415, mimetype="text/plain"
         )
-
-    if gptUsable == 1 : # it might be 0 or 1
-        if 1 in set(targetIndex[1:]):
-            # gpt 를 한 번 썼고 그 중에 내가 필요한 쿼리만 선택했을 떄
-            for index, query in enumerate(inputs['text']):
-                if index!=0:
-                    responseQueryList.append(query)
-                if index != 0 and targetIndex[index] == 1:
-                    queryList.append(inputs['text'][index].strip())
-            itemIDList = inputQuery(queryList)
-            outReason = 0
-        else : 
-            # gpt를 처음 쓸 때
-            gptResponse = gpt.get_response(inputs['text'][0])
-            gptResponse = gptResponse["choices"][0]["text"].strip()
-            print(gptResponse)
-            try : 
-                gptResponses = gptResponse.split(",")
-                gptInputs = gptResponses[1:]
-            except :
-                outReason = 1
     
-            if gptResponses[0] == "1":
-                print("gpt response is available")
-                for cnt, query in enumerate(gptInputs):
-                    if cnt < 3:
-                        responseQueryList.append(query.strip())
-                        queryList.append(query.strip())
-                itemIDList = inputQuery(queryList)
-                targetIndex = []
-                for i in range(len(queryList)):
-                    targetIndex.append(1)
-                outReason = 0
-            else :
-                outReason = 2
+    gptResponse = gpt.get_response(queryList[0])
+    gptResponse = gptResponse["choices"][0]["text"].strip()
+    try : 
+        gptResponses = gptResponse.split(",")
+    except :
+        return Response({"GPT is not available, please turn it off."}, status=424, mimetype="application/json")
+    
+    gptInputs = gptResponses[1:]
+
+    if gptResponses[0] == "1":
+        for cnt, query in enumerate(gptInputs):
+            if cnt < 3:
+                queryList.append(query.strip())
+        finalDict, sendDict = fclip.ret_queries(queryList)
+        
+        async def post_log(sendDict):
+            response = requests.post("http://127.0.0.1:5000/tests", json=sendDict)
+            return response
+        
+        response = await post_log(sendDict)
+
     else :
-        for index, query in enumerate(inputs['text']):
-            if index != 0:
-                responseQueryList.append(query)
-            if index != 0 and targetIndex[index] == 1:
-                queryList.append(inputs['text'][index].strip())
-            itemIDList = inputQuery(queryList)
-            outReason = 0
-    usedQuery = responseQueryList
-    output = {"response":outReason, "gpt_usable":gptUsable, "user_id" : user_id, "text":usedQuery, 'target_index':targetIndex, "item_ids":itemIDList, "timestamp" : timestamp}
-    
-    """
-    Response body
-    {
-        "response" : number,
-        "user_id" : number,
-        "text" : String[],
-        "target_index : Integer[],
-        "item_ids" : Integer[], # item id
-        "timestamp" : 160943 unix timestamp
-    }
-    """
-
-    return Response(response=json.dumps(output), status=200, mimetype="application/json")
+        return Response(response={"Your query is not related to Fashion."}, status=400, mimetype="application/json")
+    if response.status_code == 200:
+        output = {"user_id": user_id,
+                "text" : queryList,
+                "items":finalDict,
+                "timestamp":timestamp}
+        
+        return Response(response=json.dumps(output), status=200, mimetype="application/json")
+    else:
+        return Response(response={"Internal Server is not working."}, status=500, mimetype="application/json")
 
 
