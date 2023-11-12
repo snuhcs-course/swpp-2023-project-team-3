ad06735374a31b00f3eec03ba8ee8dd98ce8a967
covv09on9 <kwj970712@snu.ac.kr>
2023-11-12 23:03:18 +0900

    [test] testing search db APIs.

--- Backend/database/tryot/search/tests.py
+++ Backend/database/tryot/search/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SearchTestCase(TestCase):
+    def test_queryHistoryAPI(self):
+        """woojin"""
+        return True
+    def test_chatHistoryAPI(self):
+
+        return True
+    def test_saveChat(self):
+
+        return True
+    def test_saveSearch(self):
+        """woojin"""
+        return True
+    def test_saveSearchItem(self):
+        """woojin"""
+        return True
