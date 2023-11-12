40ed0845d015ed3c5dd75587682d117557ae102c
dydtjrsj21 <44419206+dydtjrsj21@users.noreply.github.com>
2023-11-12 22:23:00 +0900

    [test] test for item backend model
    Test : Django Backend Items 모델 테스트 브랜치

--- Backend/database/tryot/items/tests.py
+++ Backend/database/tryot/items/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import json
 
-# Create your tests here.
+# from rest_framework.test import APITestCase
+from .views import *
+from .models import Item, Brand, Category
+from .serializers import *
+
+client = Client()
+
+class ItemTestCase(TestCase):
+    def test_item_info(self):
+        _brand = Brand.objects.create(
+            id = 123,
+            name = "TEST_BRAND"
+        )
+        _category =Category.objects.create(
+            id = 123,
+            name = "TEST_CATEGORY"
+        )
+        item = Item.objects.create(
+            name = "TEST_CLOTHES",
+            description = "TEST_DESCRIPTION",
+            price = 10000,
+            category = _category,
+            image_url = ["TEST_URL"],
+            order_url = "TEST_URL",
+            brand = _brand,
+        )
+        pk = item.id 
+        response = client.get(f'/item/item-info/{pk}')
+        self.assertEqual(response.status_code, 200)
+
+        
