8a1cd39706bd72eb6f234a94215aff4e7f5126be
covv09on9 <kwj970712@snu.ac.kr>
2023-11-12 22:55:53 +0900

    [fix] items testing

--- Backend/database/tryot/items/tests.py
+++ Backend/database/tryot/items/tests.py
@@ -10,25 +10,29 @@ client = Client()
 
 class ItemTestCase(TestCase):
     def test_item_info(self):
-        _brand = Brand.objects.create(
+        _brand = Brand(
             id = 123,
             name = "TEST_BRAND"
         )
-        _category =Category.objects.create(
-            id = 123,
-            name = "TEST_CATEGORY"
-        )
+        _brand.save()
+        # _category =Category(
+        #     id = 123,
+        #     name = "TEST_CATEGORY",
+        #     parent = None
+        # )
+        # _category.save()
         item = Item.objects.create(
+            id = 1,
             name = "TEST_CLOTHES",
             description = "TEST_DESCRIPTION",
             price = 10000,
-            category = _category,
+            # category = cates,
             image_url = ["TEST_URL"],
             order_url = "TEST_URL",
             brand = _brand,
         )
         pk = item.id 
-        response = client.get(f'/item/item-info/{pk}')
+        response = client.get(f'/items/item-info/{pk}')
         self.assertEqual(response.status_code, 200)
 
         
