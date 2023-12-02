23d1d4e432f00dc1cc42bc296f2326825b1444c2
covv09on9 <kwj970712@snu.ac.kr>
2023-12-02 14:37:12 +0900

    [test] signup screen test

--- Frontend/TryOt/__tests__/SignUpScreen.test.tsx
+++ Frontend/TryOt/__tests__/SignUpScreen.test.tsx
@@ -3,45 +3,63 @@
  */
 
 
-import 'react-native';
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react-native';
 import { NavigationContainer } from '@react-navigation/native';
-
 import SignUpScreen from '../src/screens/SignUpScreen';
 
-it('snap shot', () => {
+const user = {
+    Email: "test@email.com",
+    Username : "testUser",
+    Password : "testPassword"
+  };
+
+describe('SignUp success', () => {
+
     jest.useFakeTimers()
-    const screen = render( 
-        <NavigationContainer>
-            <SignUpScreen />
-        </NavigationContainer>
-    );
-    const json = screen.toJSON();
-    expect(json).toMatchSnapshot();
+    test('snap shot', () => {
+        render( 
+            <NavigationContainer>
+                <SignUpScreen />
+            </NavigationContainer>
+        );
+        const json = screen.toJSON();
+        expect(json).toMatchSnapshot();
+    });
+
+    test('headers correctly', () => {
+        // jest.useFakeTimers()
+        const {getByText} = render(
+            <NavigationContainer>
+                <SignUpScreen />
+            </NavigationContainer>
+        );
+        const header = getByText('Create an Account');
+        expect(header).toBeDefined();
+    });
+
+    test('submits form', () => {
+        const {getAllByText, getByText} = render(
+            <NavigationContainer>
+                <SignUpScreen />
+            </NavigationContainer>
+        );
+        const email = getAllByText('Email')[0];
+        const username = getAllByText('Username')[0];
+        const password = getAllByText('Password')[0];
+        const confirm_password = getAllByText('Confirm Password')[0]
+        fireEvent.changeText(email, user.Email);
+        fireEvent.changeText(username, user.Username);
+        fireEvent.changeText(password, user.Password);
+        fireEvent.changeText(confirm_password, user.Password);
+
+        const onSubmit = getByText('Create Account');
+        fireEvent.press(onSubmit);
+        expect(onSubmit).toBeDefined();
+        expect(email).toBeDefined();
+        expect(username).toBeDefined();
+        expect(password).toBeDefined();
+        expect(confirm_password).toBeDefined();
+    });
+
 });
-
-it('renders correctly', () => {
-    jest.useFakeTimers();
-
-    const { getByText } = render(
-        <NavigationContainer>
-          <SignUpScreen />
-        </NavigationContainer>
-      );
-    const header = getByText('Create an Account');
-    expect(header).toBeDefined();
-  });
-
-
-// it('renders correctly', () => {
-//     jest.useFakeTimers();
-
-//     const { getByText } = render(
-//         <NavigationContainer>
-//           <SignUpScreen />
-//         </NavigationContainer>
-//       );
-//     const header = getByText('Create an Account');
-//     expect(header).toBeDefined();
-//   });
